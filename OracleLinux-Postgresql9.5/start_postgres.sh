4e350bc3106bcbf47580c741e3b18ccc7a0bd358
root <xiehonghuicn@gmail.com>
2019-04-01 08:43:01 +0000

    fix create postgres db issue

--- OracleLinux-Postgresql9.5/start_postgres.sh
+++ OracleLinux-Postgresql9.5/start_postgres.sh
@@ -3,7 +3,8 @@
 DB_NAME=${POSTGRES_DB:-}
 DB_USER=${POSTGRES_USER:-}
 DB_PASS=${POSTGRES_PASSWORD:-}
-PG_CONFDIR="/var/lib/pgsql/data"
+PG_CONFDIR="/var/lib/pgsql/9.5/data"
+export PATH=$PATH:/usr/pgsql-9.5/bin
 
 __create_user() {
   #Grant rights
@@ -21,7 +22,7 @@ if [ -n "${DB_USER}" ]; then
   fi
     echo "Creating user \"${DB_USER}\"..."
     echo "CREATE ROLE ${DB_USER} with CREATEROLE login superuser PASSWORD '${DB_PASS}';" |
-      sudo -u postgres -H postgres --single \
+      sudo -u postgres -H /usr/pgsql-9.5/bin/postgres --single \
        -c config_file=${PG_CONFDIR}/postgresql.conf -D ${PG_CONFDIR}
   
 fi
@@ -29,13 +30,13 @@ fi
 if [ -n "${DB_NAME}" ]; then
   echo "Creating database \"${DB_NAME}\"..."
   echo "CREATE DATABASE ${DB_NAME};" | \
-    sudo -u postgres -H postgres --single \
+    sudo -u postgres -H /usr/pgsql-9.5/bin/postgres --single \
      -c config_file=${PG_CONFDIR}/postgresql.conf -D ${PG_CONFDIR}
 
   if [ -n "${DB_USER}" ]; then
     echo "Granting access to database \"${DB_NAME}\" for user \"${DB_USER}\"..."
     echo "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} to ${DB_USER};" |
-      sudo -u postgres -H postgres --single \
+      sudo -u postgres -H /usr/pgsql-9.5/bin/postgres --single \
       -c config_file=${PG_CONFDIR}/postgresql.conf -D ${PG_CONFDIR}
   fi
 fi
@@ -46,7 +47,14 @@ __run_supervisor() {
 supervisord -n
 }
 
+__preparefordb() {
+   
+  /usr/bin/postgresql-setup initdb
+  chown -v postgres.postgres /var/lib/pgsql/9.5/data/postgresql.conf
+  
+}
+
 # Call all functions
+__preparefordb
 __create_user
 __run_supervisor
-
